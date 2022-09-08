87df9a2b3ab2873db6069849154dd2fd8cff57f8
Renan Araujo <ra.carvalho@unesp.br>
2022-09-08 16:55:48 -0300

    create insert function in user repository

---
+++ src/repositories/userRepository.ts
@@ -0,0 +1,21 @@
+import client from "../database/db";
+
+export interface User {
+    "id": number;
+    "email": string;
+    "password": string;
+    "createdAt": Date;
+};
+
+export type UserInsertType = Omit<User, "id" | "createdAt">;
+
+export async function insert(userData: UserInsertType) {
+    const {email, password } = userData;
+
+    await client.users.create({
+        data: {
+            email: email,
+            password: password
+        }
+    })
+};
