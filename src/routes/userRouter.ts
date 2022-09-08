9284ec7a2f1bfddb6b331b87456119940373e6d3
Renan Araujo <ra.carvalho@unesp.br>
2022-09-08 17:09:08 -0300

    create user schema and validate schema middleware

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,8 +1,10 @@
+import { insertUserSchema } from './../schemas/userSchemas';
 import { Router } from "express";
 import { createUser } from "../controllers/userController";
+import schemaValidator from "../middlewares/schemaValidator";
 
 const userRouter = Router();
 
-userRouter.post("/create", createUser);
+userRouter.post("/create", schemaValidator(insertUserSchema), createUser);
 
 export default userRouter;
