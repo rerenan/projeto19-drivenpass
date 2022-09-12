0663484747300899faefa69a0f2bc191a58f6c1c
Renan Araujo <ra.carvalho@unesp.br>
2022-09-12 20:19:01 -0300

    token validation adjustment

--- src/middlewares/tokenValidator.ts
+++ src/middlewares/tokenValidator.ts
@@ -1,15 +1,22 @@
-import jwtDecode from "jwt-decode"
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+import jwtDecode from "jwt-decode";
+
+dotenv.config();
 
 export default async function tokenValidator(req: Request, res: Response, next: NextFunction) {
     const { authorization } = req.headers;
     if(!authorization) throw {type: "Unauthorized", message: "Token invalid"};
     const token = authorization?.replace("Bearer ", "").trim();
-    
-    const tokenPayload: {userId?: number} = jwtDecode(token)
-
-    if (!tokenPayload) throw  {type: "unauthorized", message: "not authorized"};
-    res.locals.userId = tokenPayload.userId;
-
-    next();
-}
+    try{
+        const JWT_SECRET = process.env.JWT_SECRET
+        const validation = jwt.verify(token, JWT_SECRET ||"secret");
+        const tokenPayload: {userId?: number} = jwtDecode(token)
+        if (!tokenPayload) throw  {type: "unauthorized", message: "not authorized"};
+        res.locals.userId = tokenPayload.userId;
+        next();
+    }catch(err){
+        throw {type: "unauthorized", message: "not authorized"};
+    }; 
+};
